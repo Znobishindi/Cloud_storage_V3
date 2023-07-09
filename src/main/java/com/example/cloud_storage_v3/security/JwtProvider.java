975c089ade76d7fa5aff3525b1f9aed27cba9ce1
Denis Znobishin <znobishindi@yandex.ru>
2023-07-09 18:54:35 +0300

    class JwtProvider modified

--- src/main/java/com/example/cloud_storage_v3/security/JwtProvider.java
+++ src/main/java/com/example/cloud_storage_v3/security/JwtProvider.java
@@ -1,12 +1,17 @@
 package com.example.cloud_storage_v3.security;
 
 import com.example.cloud_storage_v3.entity.User;
+import com.example.cloud_storage_v3.exceptions.UserNotFoundException;
+import com.example.cloud_storage_v3.repository.UserRepository;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
@@ -21,22 +26,27 @@ import java.util.List;
 @Slf4j
 @Component
 public class JwtProvider {
-
+    private final UserRepository userRepository;
     private final SecretKey jwtAccessSecret;
     private User authUser;
 
     public User getAuthorizedUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        authUser = userRepository.findByLogin(authentication.getName()).orElseThrow(() ->
+                new UserNotFoundException("Пользователя с таким логином нет в базе данных", 0));
+
         return authUser;
     }
 
+    @Autowired
     public JwtProvider(
             @Value("${jwt.secret.access}")
-            String jwtAccessSecret){
+            String jwtAccessSecret, UserRepository userRepository) {
         this.jwtAccessSecret = Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtAccessSecret));
+        this.userRepository = userRepository;
     }
 
     public String generateAccessToken(@NonNull User user) {
-        authUser = user;
         LocalDateTime now = LocalDateTime.now();
         Instant accessExpirationInstant = now.plusMinutes(5)
                 .atZone(ZoneId.systemDefault()).toInstant();
